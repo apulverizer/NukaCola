c4cc24436d9aabcbb6e96b700ee9a1ca2d32f463
Aaron Pulver <apulverizer@gmail.com>
2017-02-04 22:54:01 -0500

    Add comments and more error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
+# -*- coding: UTF-8 -*-
 from flask import Flask
 from app.api.views import api
 import RPi.GPIO as GPIO
 
 def create_app(config):
+    """
+    Factory method to create an app instance using a config file
+    """
     app = Flask(__name__)
     app.config.from_object(config)
     app.register_blueprint(api, url_prefix='/outputs')
@@ -11,6 +15,11 @@ def create_app(config):
 
 
 def configure_outputs(app):
+    """
+    Configures the outputs usign RPi GPIO library
+    :param app: the application instance
+    :return:
+    """
     GPIO.setmode(GPIO.BOARD)
     for id, pin in app.config['OUTPUTS'].items():
         GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from flask import Blueprint, jsonify
 from flask import current_app
 import RPi.GPIO as GPIO
@@ -6,23 +7,63 @@ api = Blueprint('api', __name__)
 
 
 def output_not_configured_error():
+    """
+    Error handler when the specified pin has not been configured for use with app
+    :return: json error message with status code of 400
+    """
     return jsonify({
         "Error": {
             "Message": "The output is not configured to be set by the app"
         }
-    })
+    }), 400
 
 
 def gpio_error(e):
+    """
+    Error handler when something went wrong setting the GPIO
+    :param e: The exception
+    :return: json error message with status code 500
+    """
     return jsonify({
         "Error": {
             "Message": e.message
         }
-    })
+    }), 500
+
+
+@api.errorhandler(404)
+def page_not_found():
+    """
+    Error handler when an invalid page was requested
+    :return: json error message with status code 404
+    """
+    return jsonify({
+        "Error": {
+            "Message": "Page not found",
+        }
+    }), 404
+
+
+@api.errorhandler(500)
+def server_error():
+    """
+    Error handler for any general internal error
+    :return: json error message and status code 500
+    """
+    return jsonify({
+        "Error": {
+            "Message": "Internal error occurred"
+        }
+    }), 500
 
 
 @api.route('/<int:id>/', methods=['GET'])
 def get_output(id):
+    """
+    Returns info about an output
+    :param id: (int) the output to get info about
+    :return: json string of info or error
+    """
     if id not in current_app.config['OUTPUTS']:
         return output_not_configured_error()
     return jsonify({
@@ -33,6 +74,11 @@ def get_output(id):
 
 @api.route('/<int:id>/on', methods=['POST'])
 def set_output_on(id):
+    """
+    Sets the specified output to be on
+    :param id: (int) the output to set
+    :return: json message showing the status of the output or error message
+    """
     if id not in current_app.config['OUTPUTS']:
         return output_not_configured_error()
     try:
@@ -47,6 +93,11 @@ def set_output_on(id):
 
 @api.route('/<int:id>/off', methods=['POST'])
 def set_output_off(id):
+    """
+    Sets the specified output to be off
+    :param id: (int) the output to change
+    :return: json message showing the status of the output or error message
+    """
     if id not in current_app.config['OUTPUTS']:
         return output_not_configured_error()
     try:
