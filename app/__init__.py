5f2f1d51e6f9647737722d471ffb2fb69c949bf5
Aaron Pulver <apulverizer@gmail.com>
2017-11-23 20:08:17 +0000

    enable cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask
 from app.api.views import api
+from flask_cors import CORS
 import RPi.GPIO as GPIO
 
 def create_app(config):
@@ -8,6 +9,7 @@ def create_app(config):
     Factory method to create an app instance using a config file
     """
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config)
     app.register_blueprint(api, url_prefix='/outputs')
     configure_outputs(app)
